7f311350caba7073a9407d540f26c7f7ae545962
m-jishnu <jishnum499@gmail.com>
2023-11-08 00:36:27 +0530

    update desktop site

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -4,10 +4,22 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: "#000",
-                secondary: "#fff",
+                primary: "rgba(22, 24, 32)",
+                secondary: "#eee",
+                pop: "#D84E00",
+                t: "rgba(187, 187, 191, 0.6)",
+                p: "rgba(187, 187, 191, 0.4)",
+                box: "#2f3138",
             },
-            textColor: ["responsive", "hover", "focus", "placeholder"],
+            fontFamily: {
+                sans: ["Outfit", "sans-serif"],
+            },
+            boxShadow: {
+                custom: "0 10px 30px rgba(0, 0, 0, 0.5)",
+            },
+            lineHeight: {
+                10: "2.5rem",
+            },
         },
     },
     plugins: [],
