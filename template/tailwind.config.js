bbfcc43d0105a31306aae4e8bff84d6d3936f630
m-jishnu <jishnum499@gmail.com>
2023-11-08 14:10:50 +0530

    renamed themes

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -4,12 +4,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: "rgba(22, 24, 32)",
-                secondary: "#eee",
-                pop: "#D84E00",
-                t: "rgba(187, 187, 191, 0.6)",
-                p: "rgba(187, 187, 191, 0.4)",
-                box: "#2f3138",
+                boxColor: "rgba(22, 24, 32)",
+                button: "#D84E00",
+                labelColor: "rgba(187, 187, 191, 0.4)",
+                lbox: "#2f3138",
             },
             fontFamily: {
                 sans: ["Outfit", "sans-serif"],
