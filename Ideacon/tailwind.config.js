64ec675ef717c6b6676f2a33ffe49b76a6e451a5
Amal <Amal@Ajiths-MacBook-Air.local>
2023-11-09 14:44:31 +0530

    completed pubg

---
+++ Ideacon/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                boxColor: "#020003",
+                button: "#FFC107",
+                labelColor: "#999999",
+                lbox: "#031F40",
+            },
+            fontFamily: {
+                sans: ["Outfit", "sans-serif"],
+            },
+            boxShadow: {
+                custom: "0 10px 30px rgba(0, 0, 0, 0.5)",
+            },
+            lineHeight: {
+                10: "2.5rem",
+            },
+        },
+    },
+    plugins: [],
+};
+("https://media.contentapi.ea.com/content/dam/gin/images/2019/06/fifa20-gin-keyart.jpg.adapt.crop1x1.767p.jpg");
