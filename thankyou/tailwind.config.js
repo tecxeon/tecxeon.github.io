ef51dfbb893155db29db2b29c22f920b4ad7439f
tecxeon <149892869+tecxeon@users.noreply.github.com>
2023-11-09 22:29:38 +0530

    Add files via upload

---
+++ thankyou/tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          boxColor: '',
+          '3F8961': '#3F8961',
+        },
+        backgroundImage: {
+          'tecxeonW': 'url(tecxeonW.png)',
+        },
+        borderRadius: {
+          '32': '32px',
+          '10': '10px',
+          '20': '20px',
+          '2': '2px',
+          '5': '5px',
+        },
+        fontSize: {
+          '12': '12px',
+        },
+        fontFamily: {
+          'Inter̈̈': ['Inter̈̈', 'sans'],
+          'Outfit': ['Outfit', 'sans'],
+        },
+        zIndex: {
+          '50': 50,
+        },
+        backdropBlur: {
+          'xl': '20px',
+          'sm': '4px',
+        },
+        width: {
+          '72': '18rem',
+          '291': '72.75rem',
+        },
+        height: {
+          '71': '17.75rem',
+          '185': '46.25rem',
+        },
+        maxWidth: {
+          '603': '150.75rem',
+          '328': '82rem',
+        },
+        minWidth: {
+          '328': '82rem',
+        },
+        letterSpacing: {
+          '0.20': '0.20em',
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
